8578d9c34d2404733eb0d3c732ce5297ecbd813d
xaservii <64789072+xaservii@users.noreply.github.com>
2021-01-22 20:44:02 +0800

    animations

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+gsap.from('.col1', {opacity: 0, duration: 1.2, x: -50, ease: 'Power2.easeInOut'})
+gsap.from('.col2', {opacity: 0, duration: 1.2, x: 50, ease: 'Power2.easeInOut'})
+gsap.from('#jumbo-p', {opacity: 0, delay: 0.75, duration: 1.2, y: 70, ease: 'Power2.easeInOut'})
